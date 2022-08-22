1457a7a812aa30b87b09a7f4c99002cad2fb3e30
sangwan2016 <sangwan2016@kaist.ac.kr>
2022-08-22 15:57:34 +0900

    1차

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
         <footer>
             <Container>
                 <Row>
-                    <Col className="text-center py-3">Copyright &copy; ProShop</Col>
+                    <Col className="text-center py-3">Copyright &copy; 1bunga</Col>
                 </Row>
             </Container>
         </footer>
